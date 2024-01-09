181fe2b1f062aa1cecfa4f850345acfef44e2be6
begarland <brandy.e.garland@gmail.com>
2024-01-08 23:35:26 -0700

    sample

---
+++ src/OtherApp.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function OtherApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Woooooo it worked
+      </header>
+    </div>
+  );
+}
+
+export default OtherApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import OtherApp from "./OtherApp";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (process.env.REACT_APP_TEST_ENV === "other") {
+  const other = ReactDOM.createRoot(
+    document.getElementById("other") as HTMLElement
+  );
+  other.render(
+    <React.StrictMode>
+      <OtherApp />
+    </React.StrictMode>
+  );
+} else {
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
